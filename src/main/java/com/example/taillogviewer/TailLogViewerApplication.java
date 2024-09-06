ff67bc1268f468afbd660de041159f035d8c86c9
Bhaskar Bantupalli <bhaskarbnms0@gmail.com>
2024-09-06 23:00:42 +0530

    starter project, web,websocket deps

---
+++ src/main/java/com/example/taillogviewer/TailLogViewerApplication.java
@@ -0,0 +1,13 @@
+package com.example.taillogviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TailLogViewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TailLogViewerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taillogviewer/TailLogViewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taillogviewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TailLogViewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
